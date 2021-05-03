b66582a359bf5124b8f8c80ff2ae80d83043ebb8
sawon choi <sawon49@naver.com>
2021-05-04 02:23:07 +0900

    5/4

--- algorithm/src/algorithm/Main.java
+++ algorithm/src/algorithm/Main.java
@@ -10,28 +10,8 @@ public class Main {
 	 public static void main(String[] args)
 	 {
 		 
-		 String sentence = sc.nextLine();
 		 
 		 
-		 int l = 0, r=sentence.length()-1;
-		 
-		 while(l<r)
-		 {
-			 
-			 while(sentence.charAt(l)!='(' && l<r) 
-			{
-				 l++;
-			}
-			 while(sentence.charAt(r)!=')'&& l<r) r--;
-			 
-			 if(l>r) break;
-			 
-			 System.out.println(sentence.substring(0,l) + sentence.substring(l+1,r) +sentence.substring(r+1));
-			 l++;
-			 r--;
-			 
-			 
-		 }
 		 
 	 }
 
